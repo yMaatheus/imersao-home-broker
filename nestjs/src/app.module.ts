b77ce90b67910d66142438caa6de3bfa90b2e7cb
Matheus <ymatheus.h@gmail.com>
2023-06-27 11:52:04 -0300

    feat(nestjs): :sparkles: implements prisma setup

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { AssetsModule } from './assets/assets.module';
+import { WalletsModule } from './wallets/wallets.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, AssetsModule, WalletsModule],
   controllers: [AppController],
   providers: [AppService],
 })
