0fc3fff6e4640fa024ccdbfe610fa8fd56629714
Matheus <ymatheus.h@gmail.com>
2023-06-28 16:08:13 -0300

    style(nestjs):

--- nestjs/src/wallets/wallet-assets/wallet-assets.controller.ts
+++ nestjs/src/wallets/wallet-assets/wallet-assets.controller.ts
@@ -7,7 +7,6 @@ export class WalletAssetsController {
 
   @Get()
   all(@Param('wallet_id') wallet_id: string) {
-    console.log('wallet_id: ', wallet_id);
     return this.walletAssetsService.all({ wallet_id });
   }
 
