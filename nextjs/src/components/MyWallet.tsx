f942c8e362ca213b2d3053a9ef4f9ce11fcc0cfd
Matheus <ymatheus.h@gmail.com>
2023-07-01 05:43:16 +0000

    feat(nextjs): :sparkles: implements table assets at my wallet component

--- nextjs/src/components/MyWallet.tsx
+++ nextjs/src/components/MyWallet.tsx
@@ -1,4 +1,6 @@
 import { WalletAsset } from "@/app/models";
+import { Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "@/components/flowbite-components"
+import Link from "next/link";
 
 async function getWalletAssets(wallet_id: string): Promise<WalletAsset[]> {
   const response = await fetch(
@@ -16,13 +18,34 @@ async function getWalletAssets(wallet_id: string): Promise<WalletAsset[]> {
 export async function MyWallet(props: { wallet_id: string }) {
   const walletAssets = await getWalletAssets(props.wallet_id);
   return (
-      <ul>
-        {walletAssets.map((walletAsset) => (
-          <li key={walletAsset.id}>
-            {walletAsset.Asset.id} - {walletAsset.shares} - R${" "}
-            {walletAsset.Asset.price}
-          </li>
-        ))}
-      </ul>
+    <Table>
+    <TableHead>
+      <TableHeadCell>Nome</TableHeadCell>
+      <TableHeadCell>Preço R$</TableHeadCell>
+      <TableHeadCell>Quant.</TableHeadCell>
+      <TableHeadCell>
+        <span className="sr-only">Comprar/Vender</span>
+      </TableHeadCell>
+    </TableHead>
+    <TableBody className="divide-y">
+      {walletAssets!.map((walletAsset, key) => (
+        <TableRow className="border-gray-700 bg-gray-800" key={key}>
+          <TableCell className="whitespace-nowrap font-medium text-white">
+            {walletAsset.Asset.id} ({walletAsset.Asset.symbol})
+          </TableCell>
+          <TableCell>{walletAsset.Asset.price}</TableCell>
+          <TableCell>{walletAsset.shares}</TableCell>
+          <TableCell>
+            <Link
+              className="font-medium hover:underline text-cyan-500"
+              href={`/${props.wallet_id}/home-broker/${walletAsset.Asset.id}`}
+            >
+              Comprar/Vender
+            </Link>
+          </TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
+  </Table>
   );
 }
---
+++ nextjs/src/components/flowbite-components.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Table } from "flowbite-react"
+
+export * from 'flowbite-react'
+
+export const TableHead = Table.Head
+export const TableHeadCell = Table.HeadCell
+export const TableBody = Table.Body
+export const TableRow = Table.Row
+export const TableCell = Table.Cell
