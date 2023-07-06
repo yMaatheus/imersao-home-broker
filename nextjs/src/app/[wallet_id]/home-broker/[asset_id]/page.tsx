b1d629107747afa682a668e89040cd828f393540
Matheus <ymatheus.h@gmail.com>
2023-07-06 13:44:55 +0000

    feat(nextjs): :sparkles: implements style to home broker page and chart asset component

--- nextjs/src/app/[wallet_id]/home-broker/[asset_id]/page.tsx
+++ nextjs/src/app/[wallet_id]/home-broker/[asset_id]/page.tsx
@@ -1,6 +1,7 @@
+import { ChartComponent } from '@/components/ChartComponent'
 import { MyOrders } from '@/components/MyOrders'
 import { OrderForm } from '@/components/OrderForm'
-import { TabsGroup, TabsItem } from '@/components/flowbite-components'
+import { TabsGroup, TabsItem, Card } from '@/components/flowbite-components'
 import { HiArrowUp, HiShoppingCart } from '@/components/react-itens/hi'
 
 export default async function HomeBrokerPage({
@@ -9,34 +10,56 @@ export default async function HomeBrokerPage({
   params: { wallet_id: string; asset_id: string };
 }) {
   return (
-    <div>
-      <h1>Home Broker</h1>
-      <div className="flex flex-row">
-        <div className="flex flex-col">
+    <main className="flex flex-grow flex-col container mx-auto p-2 ">
+      <article className="format format-invert">
+        <h1>Home broker - {params.asset_id}</h1>
+      </article>
+      <div className="grid grid-cols-5 flex-grow gap-2 mt-2">
+        <div className="col-span-2">
           <div>
-            <TabsGroup aria-label="Default tabs" style="pills">
-              <TabsItem active title="Comprar" icon={HiShoppingCart}>
-                <OrderForm
-                  asset_id={params.asset_id}
-                  wallet_id={params.wallet_id}
-                  type="BUY"
-                />
-              </TabsItem>
-              <TabsItem title="Vender" icon={HiArrowUp}>
-                <OrderForm
-                  asset_id={params.asset_id}
-                  wallet_id={params.wallet_id}
-                  type="SELL"
-                />
-              </TabsItem>
-            </TabsGroup>
+            <Card
+              theme={{
+                root: {
+                  children:
+                    'flex h-full flex-col justify-center gap-4 py-4 px-2',
+                },
+              }}
+            >
+              <TabsGroup aria-label="Default tabs" style="pills">
+                <TabsItem active title="Comprar" icon={HiShoppingCart}>
+                  <OrderForm
+                    asset_id={params.asset_id}
+                    wallet_id={params.wallet_id}
+                    type="BUY"
+                  />
+                </TabsItem>
+                <TabsItem title="Vender" icon={HiArrowUp}>
+                  <OrderForm
+                    asset_id={params.asset_id}
+                    wallet_id={params.wallet_id}
+                    type="SELL"
+                  />
+                </TabsItem>
+              </TabsGroup>
+            </Card>
           </div>
-          <div>
-            <MyOrders wallet_id={params.wallet_id} />
+          <div className="mt-2">
+            <Card
+              theme={{
+                root: {
+                  children:
+                    'flex h-full flex-col justify-center gap-4 py-4 px-2',
+                },
+              }}
+            >
+              <MyOrders wallet_id={params.wallet_id} />
+            </Card>
           </div>
         </div>
-        <div>gráfico</div>
+        <div className="col-span-3 flex flex-grow">
+          <ChartComponent header='Asset 1 - R$ 100' />
+        </div>
       </div>
-    </div>
+    </main>
   )
 }
