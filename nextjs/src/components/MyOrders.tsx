c971e397db6cf11e0fa417cf712a1e4489a23978
Matheus <ymatheus.h@gmail.com>
2023-07-06 12:32:10 +0000

    feat(nextjs): :sparkles: implements style at my orders component

--- nextjs/src/components/MyOrders.tsx
+++ nextjs/src/components/MyOrders.tsx
@@ -1,4 +1,13 @@
 import { Order } from '@/app/models'
+import {
+  Table,
+  TableHead,
+  TableHeadCell,
+  TableBody,
+  TableRow,
+  TableCell,
+  Badge,
+} from './flowbite-components'
 
 async function getOrders(wallet_id: string): Promise<Order[]> {
   const response = await fetch(
@@ -17,12 +26,36 @@ async function getOrders(wallet_id: string): Promise<Order[]> {
 export async function MyOrders(props: { wallet_id: string }) {
   const orders = await getOrders(props.wallet_id)
   return (
-    <ul>
-      {orders?.map((order) => (
-        <li key={order.id}>
-          {order.Asset.id} - {order.shares} - R${order.price} - {order.status}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <article className="format format-invert">
+        <h2>Minhas ordens</h2>
+      </article>
+      <Table className="mt-2">
+        <TableHead>
+          <TableHeadCell>asset_id</TableHeadCell>
+          <TableHeadCell>quant.</TableHeadCell>
+          <TableHeadCell>price</TableHeadCell>
+          <TableHeadCell>tipo</TableHeadCell>
+          <TableHeadCell>status</TableHeadCell>
+        </TableHead>
+        <TableBody>
+          {orders.map((order, key) => (
+            <TableRow className=" border-gray-700 bg-gray-800" key={key}>
+              <TableCell className="whitespace-nowrap font-medium text-white">
+                {order.Asset.id}
+              </TableCell>
+              <TableCell>{order.shares}</TableCell>
+              <TableCell>{order.price}</TableCell>
+              <TableCell>
+                <Badge>{order.type}</Badge>
+              </TableCell>
+              <TableCell>
+                <Badge>{order.status}</Badge>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
   )
 }
