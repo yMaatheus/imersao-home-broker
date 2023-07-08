8d298dc8eb04934e5f0bf583dbe633c41b0ddc31
Matheus <ymatheus.h@gmail.com>
2023-07-08 13:54:22 +0000

    feat(nestjs): :sparkles: create assets daily routes with services

--- nestjs/src/assets/assets.module.ts
+++ nestjs/src/assets/assets.module.ts
@@ -3,17 +3,22 @@ import { AssetsService } from './assets.service';
 import { AssetsController } from './assets.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Asset, AssetSchema } from './asset.schema';
+import {
+  AssetDaily,
+  AssetDailySchema,
+} from './assets-daily/asset-daily.schema';
+import { AssetsDailyController } from './assets-daily/assets-daily.controller';
+import { AssetsDailyService } from './assets-daily/assets-daily.service';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      {
-        name: Asset.name,
-        schema: AssetSchema,
-      },
+      { name: Asset.name, schema: AssetSchema },
+      { name: AssetDaily.name, schema: AssetDailySchema },
     ]),
   ],
-  controllers: [AssetsController],
-  providers: [AssetsService],
+  controllers: [AssetsController, AssetsDailyController],
+  providers: [AssetsService, AssetsDailyService],
+  exports: [AssetsService],
 })
 export class AssetsModule {}
