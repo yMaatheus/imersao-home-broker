81f218d6efdf14f8f1ed1a8f87d527a6e95e6bbd
Matheus <ymatheus.h@gmail.com>
2023-07-10 18:06:11 +0000

    feat(nextjs): :sparkles: create assert chart component

--- nextjs/src/components/ChartComponent.tsx
+++ nextjs/src/components/ChartComponent.tsx
@@ -43,7 +43,6 @@ export const ChartComponent = forwardRef<
 >((props, ref) => {
   const chartContainerRef = useRef() as MutableRefObject<HTMLDivElement>
   const chartRef = useRef({
-    _api: null as null | ReturnType<typeof createChart>,
     api() {
       if (!this._api) {
         this._api = createChart(chartContainerRef.current, {
@@ -53,7 +52,7 @@ export const ChartComponent = forwardRef<
         })
         this._api.timeScale().fitContent()
       }
-      return this._api!
+      return this._api
     },
     free() {
       if (this._api) {
@@ -64,11 +63,8 @@ export const ChartComponent = forwardRef<
   const seriesRef = useRef() as MutableRefObject<ISeriesApi<'Area'>>
 
   useImperativeHandle(ref, () => ({
-    update: (data: { time: number; value: number }) => {
-      seriesRef.current.update({
-        time: data.time.toString(),
-        value: data.value,
-      })
+    update: (data: { time: string; value: number }) => {
+      seriesRef.current.update(data)
     },
   }))
 
@@ -78,18 +74,18 @@ export const ChartComponent = forwardRef<
       topColor: colors.areaTopColor,
       bottomColor: colors.areaBottomColor,
     })
-    seriesRef.current.setData([
-      { time: '2018-12-22', value: 32.51 },
-      { time: '2018-12-23', value: 31.11 },
-      { time: '2018-12-24', value: 27.02 },
-      { time: '2018-12-25', value: 27.32 },
-      { time: '2018-12-26', value: 25.17 },
-      { time: '2018-12-27', value: 28.89 },
-      { time: '2018-12-28', value: 25.46 },
-      { time: '2018-12-29', value: 23.92 },
-      { time: '2018-12-30', value: 22.68 },
-      { time: '2018-12-31', value: 22.67 },
-    ])
+    // seriesRef.current.setData([
+    //   { time: "2018-12-22", value: 32.51 },
+    //   { time: "2018-12-23", value: 31.11 },
+    //   { time: "2018-12-24", value: 27.02 },
+    //   { time: "2018-12-25", value: 27.32 },
+    //   { time: "2018-12-26", value: 25.17 },
+    //   { time: "2018-12-27", value: 28.89 },
+    //   { time: "2018-12-28", value: 25.46 },
+    //   { time: "2018-12-29", value: 23.92 },
+    //   { time: "2018-12-30", value: 22.68 },
+    //   { time: "2018-12-31", value: 22.67 },
+    // ]);
   }, [])
 
   useLayoutEffect(() => {
